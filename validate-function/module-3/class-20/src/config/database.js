aca65a2173d41d628f04a957c2c0f289db93bdd3
Rayyan-123 <161798434+Rayyan-123@users.noreply.github.com>
2025-09-15 01:55:15 +0500

    Update database.js

--- validate-function/module-3/class-20/src/config/database.js
+++ validate-function/module-3/class-20/src/config/database.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-
-const uri = 'mongodb+srv://Database:aliaftab321@cluster0.51l7ww2.mongodb.net/UniqueEmail'
+'
 
 async function connectDB() {
     await mongoose.connect(uri)
